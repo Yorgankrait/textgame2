0c05b2941c5a3deac1dba9f3235c7ac320cf0246
Fiikus <steptimus@gmail.com>
2024-11-29 16:48:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+class test:
+    def __init__(self,name,item,hp):
+        self.name = name
+        self.item = item
+        self.hp = hp
+        print(f"очень прятно {name} ваши предметы {item}")
+    def __str__(self):
+        return(
+                f"у вас теперь {self.hp} хп"
+            )
+a = test(
+    "Fiikus",
+    "Меч",
+    100
+)
+print(a)
